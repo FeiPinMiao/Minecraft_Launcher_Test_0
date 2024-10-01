9e13ee06f4022809d067591c32dab26ad709e404
FeiPinMiao <753278917@qq.com>
2024-10-01 11:24:40 +0800

    Test
    Signed-off-by: FeiPinMiao <753278917@qq.com>

---
+++ Main/Animation/Animation.cs
@@ -0,0 +1,9 @@
+namespace Animation;
+
+class AnimationProgram
+{
+	public static void Init()
+	{
+		Console.WriteLine("Animation:: State: Init   OutMessage: AnimationInit");
+	}
+}
---
+++ Main/Main.cs
@@ -0,0 +1,12 @@
+using Animation;
+
+namespace Main;
+
+class MainProgram
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Main:: State: Init   OutMessage: MainInit");
+		AnimationProgram.Init();
+	}
+}
